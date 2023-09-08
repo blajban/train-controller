6e9c795f89317edc053dbc57c65191275418eada
Erik Sjöberg <sjoerik@gmail.com>
2023-09-08 09:27:34 +0200

    Adding global style and main app containers

---
+++ frontend/src/components/Delayed.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+import styled from "styled-components";
+
+function Delayed() {
+
+  return (
+    <p>Här ska listan på tågen in</p>
+  );
+}
+
+export default Delayed;
