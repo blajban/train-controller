1d8fadc362429bffdc6799ce04fc83733685aa4c
Erik Sjöberg <sjoerik@gmail.com>
2023-09-08 10:23:26 +0200

    Added basic delayed style with placeholder data

--- frontend/src/components/Delayed.js
+++ frontend/src/components/Delayed.js
@@ -2,10 +2,112 @@ import { useState, useEffect } from 'react'
 
 import styled from "styled-components";
 
-function Delayed() {
+import Train from './Train';
 
+/*
+
+fetch("http://localhost:1337/delayed")
+        .then((response) => response.json())
+        .then(function(result) {
+            return renderDelayedTable(result.data, delayed);
+        });
+
+
+        function renderDelayedTable(data, table) {
+    data.forEach((item) => {
+        let element = document.createElement("div");
+
+        element.innerHTML = `
+            <div class="train-number">
+                ${item.OperationalTrainNumber}
+            </div>
+            <div class="current-station">
+                <div>${item.LocationSignature}</div>
+                <div>${item.FromLocation ? item.FromLocation[0].LocationName + " -> " : ""} ${item.ToLocation ? item.ToLocation[0].LocationName : ""}</div>
+            </div>
+            <div class="delay">
+                ${outputDelay(item)}
+            </div>`;
+
+        element.addEventListener("click", function() {
+            renderTicketView(item);
+        });
+
+        table.appendChild(element);
+    });
+}
+*/
+
+const DelayedTrains = styled.div`
+  display: flex;
+  flex-direction: column;
+  }
+`;
+
+
+// Placeholder data
+const train1 = {
+  OperationalTrainNumber: "Tåg 1",
+  LocationSignature: "H",
+  FromLocation: [
+    {
+      LocationName: "från..."
+    }
+  ],
+  ToLocation: [
+    {
+      LocationName: "till..."
+    }
+  ]
+};
+
+const train2 = {
+  OperationalTrainNumber: "Tåg 2",
+  LocationSignature: "H",
+  FromLocation: [
+    {
+      LocationName: "från..."
+    }
+  ],
+  ToLocation: [
+    {
+      LocationName: "till..."
+    }
+  ]
+};
+
+const train3 = {
+  OperationalTrainNumber: "Tåg 2",
+  LocationSignature: "H",
+  FromLocation: [
+    {
+      LocationName: "från..."
+    }
+  ],
+  ToLocation: [
+    {
+      LocationName: "till..."
+    }
+  ]
+};
+
+function Delayed() {
   return (
-    <p>Här ska listan på tågen in</p>
+    <div class="delayed">
+      <h1>Försenade tåg</h1>
+      <DelayedTrains>
+        <Train
+          train={train1}
+        />
+        <Train
+          train={train2}
+        />
+        <Train
+          train={train3}
+        />
+
+      </DelayedTrains>
+  </div>
   );
 }
 
---
+++ frontend/src/components/Train.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+
+import styled from "styled-components";
+
+const TrainNumber = styled.div`
+    font-size: 2rem;
+    font-weight: bold;
+    width: 30%;
+`;
+
+const CurrentStation = styled.div`
+    width: 30%;
+`;
+
+function outputDelay(item) {
+    /*
+        let advertised = new Date(item.AdvertisedTimeAtLocation);
+        let estimated = new Date(item.EstimatedTimeAtLocation);
+
+        const diff = Math.abs(estimated - advertised);
+
+        return Math.floor(diff / (1000 * 60)) + " minuter";
+    */
+   return "Försening";
+}
+
+function TrainItem({ train, ...restProps }) {
+  return (
+    <div {...restProps}>
+      <TrainNumber>{train.OperationalTrainNumber}</TrainNumber>
+      <CurrentStation>
+                <div>{train.LocationSignature}</div>
+                <div>{train.FromLocation ? train.FromLocation[0].LocationName + " -> " : ""} {train.ToLocation ? train.ToLocation[0].LocationName : ""}</div>
+      </CurrentStation>
+
+    <div>
+        {outputDelay(train)}
+    </div>
+      
+    
+    </div>
+    );
+}
+
+const StyledTrain = styled(TrainItem)`
+  display: flex;
+  flex-direction: row;
+  border-top: 1px solid #ccc;
+  padding: 0.2rem 0.8rem;
+  align-items: center;
+  cursor: pointer;
+
+  &:nth-of-type(2n) {
+    background-color: #eee;
+  }
+`;
+
+function Train({ train }) {
+  return (
+    <StyledTrain 
+        train={train}
+    />
+  );
+}
+
+export default Train;
