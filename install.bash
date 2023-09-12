2f6212eaa5f0b8bcbba55f9fbcf6732f701059e7
Erik Sjöberg <sjoerik@gmail.com>
2023-09-12 22:36:08 +0200

    Updated install script and instructions

--- install.bash
+++ install.bash
@@ -10,7 +10,10 @@ cd "$DIR/backend/"
 npm install
 ./db/reset_db.bash
 cp .env-example .env
+
+# Change to frontend directory
+cd "$DIR/frontend/"
+npm install
+
 echo
 echo "--- Don't forget to change api key in /backend/.env ---"
-
-#cd "$DIR"
