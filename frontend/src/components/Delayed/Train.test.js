be8d83d50a76737adf8241f9cc3f517c8ba43dc5
Erik Sjöberg <sjoerik@gmail.com>
2023-10-23 21:37:15 +0200

    Added trains test to frontend

--- frontend/src/components/Delayed/Train.test.js
+++ frontend/src/components/Delayed/Train.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import Train from './Train';
 import UserContext from '../../contexts/UserContext';
 import DelayedContext from '../../contexts/DelayedContext';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('./Delay', () => {
   return function MockedDelay() {
@@ -11,6 +12,106 @@ jest.mock('./Delay', () => {
 });
 
 describe('<Train />', () => {
+  it('renders train details and handles click to open ticket', () => {
+    const handleClick = jest.fn();
+    const mockIsLoggedIn = true;
+
+    const mockTrainData = {
+      OperationalTrainNumber: '12345',
+      LocationSignature: 'AA',
+      FromLocation: [{ LocationName: 'Start' }],
+      ToLocation: [{ LocationName: 'End' }],
+    };
+
+    render(
+      <DelayedContext.Provider value={{ 
+        delayedTrains: [], 
+        setDelayedTrains: jest.fn(), 
+        selectedTrain: null, 
+        setSelectedTrain: jest.fn(), 
+        trainsWithPosition: [], 
+        setTrainsWithPosition: jest.fn() 
+      }}>
+      <UserContext.Provider value={{ isLoggedIn: mockIsLoggedIn }}>
+        <Train train={mockTrainData} onClick={handleClick} />
+      </UserContext.Provider>
+      </DelayedContext.Provider>
+    );
+
+
+    expect(screen.getByText('12345')).toBeInTheDocument();
+    expect(screen.getByText('Mocked Delay')).toBeInTheDocument();
+    expect(screen.getByText('AA')).toBeInTheDocument();
+    expect(screen.getByText('Start -> End')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('+'));
+    expect(handleClick).toHaveBeenCalled();
+  });
+
+  it('renders train details and handles click to show train on map', () => {
+    let selectedTrain = null;
+    const mockIsLoggedIn = true;
+
+    const mockTrainData = {
+      OperationalTrainNumber: '12345',
+      LocationSignature: 'AA',
+      FromLocation: [{ LocationName: 'Start' }],
+      ToLocation: [{ LocationName: 'End' }],
+    };
+
+    render(
+      <DelayedContext.Provider value={{ 
+        delayedTrains: [], 
+        setDelayedTrains: jest.fn(), 
+        selectedTrain: selectedTrain, 
+        setSelectedTrain: (arg) => { selectedTrain = arg; },
+        trainsWithPosition: [ '12345'], 
+        setTrainsWithPosition: jest.fn() 
+      }}>
+      <UserContext.Provider value={{ isLoggedIn: mockIsLoggedIn }}>
+        <Train train={mockTrainData} onClick={() => {}} />
+      </UserContext.Provider>
+      </DelayedContext.Provider>
+    );
+
+    expect(screen.getByText(/Visa på karta/)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText(/Visa på karta/));
+    expect(selectedTrain).toBe(mockTrainData);
+  });
+
+  it('renders train details and handles click to show all trains on map', () => {
+    const mockIsLoggedIn = true;
+    const mockTrainData = {
+      OperationalTrainNumber: '12345',
+      LocationSignature: 'AA',
+      FromLocation: [{ LocationName: 'Start' }],
+      ToLocation: [{ LocationName: 'End' }],
+    };
+
+    let selectedTrain = mockTrainData;
+
+    render(
+      <DelayedContext.Provider value={{
+        delayedTrains: [], 
+        setDelayedTrains: jest.fn(), 
+        selectedTrain: selectedTrain, 
+        setSelectedTrain: (arg) => { selectedTrain = arg; },
+        trainsWithPosition: [ '12345'], 
+        setTrainsWithPosition: jest.fn() 
+      }}>
+      <UserContext.Provider value={{ isLoggedIn: mockIsLoggedIn }}>
+        <Train train={mockTrainData} onClick={() => {}} />
+      </UserContext.Provider>
+      </DelayedContext.Provider>
+    );
+
+    expect(screen.getByText(/Visa alla/)).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText(/Visa alla/));
+    expect(selectedTrain).toBe(null);
+  });
+
   it('renders train details and handles click', () => {
     const handleClick = jest.fn();
     const mockIsLoggedIn = true;
